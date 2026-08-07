0008a58460b25fc1493e80b7937642da63ea2a6f
Coco29dev <10059@holbertonstudents.com>
2026-08-07 21:10:33 +0200

    add task 2 Embedding expressions, functions

---
+++ react_intro/task_1/dashboard/src/App.spec.js
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('App', () => {
+  test('renders an h1 with the text School Dashboard', () => {
+    render(<App />);
+    expect(
+      screen.getByRole('heading', { level: 1, name: /school dashboard/i })
+    ).toBeInTheDocument();
+  });
+
+  test('renders the expected text in the app-body and app-footer paragraphs', () => {
+    render(<App />);
+    expect(screen.getByText(/login to access the full dashboard/i)).toBeInTheDocument();
+    expect(
+      screen.getByText(/copyright \d{4} holberton school main dashboard/i)
+    ).toBeInTheDocument();
+  });
+
+  test('renders an image', () => {
+    render(<App />);
+    expect(screen.getByAltText(/holberton logo/i)).toBeInTheDocument();
+  });
+});
